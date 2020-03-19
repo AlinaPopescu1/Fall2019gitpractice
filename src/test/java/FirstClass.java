6c4d8c0a69b44daf8b959da5c6a570eed2a2d4a1
AlinaPopescu1 <58535557+AlinaPopescu1@users.noreply.github.com>
2020-03-18 21:15:46 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,7 +3,8 @@ public class FirstClass {
         System.out.println("Hello world");
         System.out.println("Change one");
         System.out.println("Change two");
-         System.out.println("Someone");
+        System.out.println("Someone");
+        System.out.println("Change three");
         
     }
 }
